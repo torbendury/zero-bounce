b6a4691e8303113ee19bf48ec9e1dfd11375db90
Torben Dury <94752317+torbendury@users.noreply.github.com>
2022-11-04 18:42:55 +0100

    upd: `data-service` CORS allow all origins

--- backend/data-service/src/main.py
+++ backend/data-service/src/main.py
@@ -6,11 +6,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["http://localhost:5173", "http://localhost"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
