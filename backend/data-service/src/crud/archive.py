7db97853812e7c5837708603f31e14373dfa90e3
Torben Dury <94752317+torbendury@users.noreply.github.com>
2022-11-11 18:41:57 +0100

    upd: use asynchronous APIs

--- backend/data-service/src/crud/archive.py
+++ backend/data-service/src/crud/archive.py
@@ -13,56 +13,56 @@ from schemas import CategoryBase, CategoryCreate, Category, EntryBase, EntryCrea
 ############### CATEGORIES
 
 
-def get_categories(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Categories).offset(skip).limit(limit).all()
+async def get_categories(db: Session, skip: int = 0, limit: int = 100):
+    return await db.query(models.Categories).offset(skip).limit(limit).all()
 
 
-def get_category(db: Session, category_id: int):
-    return db.query(models.Categories).filter(models.Categories.id == category_id).first()
+async def get_category(db: Session, category_id: int):
+    return await db.query(models.Categories).filter(models.Categories.id == category_id).first()
 
 
-def create_category(db: Session, category: CategoryCreate):
+async def create_category(db: Session, category: CategoryCreate):
     db_category = models.Categories(name=category.name)
     db.add(db_category)
     db.commit()
     db.refresh(db_category)
-    return db_category
+    return await db_category
 
 
-def delete_category():
+async def delete_category():
     raise Exception("Implementation missing for deleting archive categories.")
 
 
-def update_category():
+async def update_category():
     raise Exception("Implementation missing for updating archive categories.")
 
 
 ############### ENTRIES
 
 
-def get_entries(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Entries).offset(skip).limit(limit).all()
+async def get_entries(db: Session, skip: int = 0, limit: int = 100):
+    return await db.query(models.Entries).offset(skip).limit(limit).all()
 
 
-def get_entry(db: Session, entry_id: int):
-    return db.query(models.Entries).filter(models.Entries.id == entry_id).first()
+async def get_entry(db: Session, entry_id: int):
+    return await db.query(models.Entries).filter(models.Entries.id == entry_id).first()
 
 
-def get_category_entries(db: Session, category_id: int):
-    return db.query(models.Entries).filter(models.Entries.category_id == category_id).all()
+async def get_category_entries(db: Session, category_id: int):
+    return await db.query(models.Entries).filter(models.Entries.category_id == category_id).all()
 
 
-def create_entry(db: Session, entry: EntryCreate):
+async def create_entry(db: Session, entry: EntryCreate):
     db_entry = models.Entries(category_id=entry.category_id, visible_to_player=entry.visible_to_player, name=entry.name)
     db.add(db_entry)
     db.commit()
     db.refresh(db_entry)
-    return db_entry
+    return await db_entry
 
 
-def delete_entry():
+async def delete_entry():
     raise Exception("Implementation missing for deleting archive category entries.")
 
 
-def update_entry():
+async def update_entry():
     raise Exception("Implementation missing for updating archive category entries.")
--- backend/data-service/src/routers/archive.py
+++ backend/data-service/src/routers/archive.py
@@ -10,13 +10,13 @@ router = APIRouter()
 
 
 @router.get("/categories", status_code=200, response_model=list[schemas.Category])
-def read_categories(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
+async def read_categories(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
     db_categories = archive.get_categories(db=db, skip=skip, limit=limit)
     return db_categories
 
 
 @router.get("/categories/{category_id}", status_code=200, response_model=schemas.Category)
-def read_category(category_id: int, response: Response, db: Session = Depends(database.get_db)):
+async def read_category(category_id: int, response: Response, db: Session = Depends(database.get_db)):
     db_category = archive.get_category(db=db, category_id=category_id)
     if db_category is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
@@ -24,7 +24,7 @@ def read_category(category_id: int, response: Response, db: Session = Depends(da
 
 
 @router.post("/categories/", response_model=schemas.Category)
-def create_category(category: schemas.CategoryCreate, db: Session = Depends(database.get_db)):
+async def create_category(category: schemas.CategoryCreate, db: Session = Depends(database.get_db)):
     return archive.create_category(db=db, category=category)
 
 
@@ -32,13 +32,13 @@ def create_category(category: schemas.CategoryCreate, db: Session = Depends(data
 
 
 @router.get("/categories/{category_id}/entries", status_code=200, response_model=list[schemas.Entry])
-def read_category_entries(category_id: int, response: Response, db: Session = Depends(database.get_db)):
+async def read_category_entries(category_id: int, response: Response, db: Session = Depends(database.get_db)):
     db_entries = archive.get_category_entries(db=db, category_id=category_id)
     return db_entries
 
 
 @router.get("/entries/{entry_id}", status_code=200, response_model=schemas.Entry)
-def read_category_entry(entry_id: int, response: Response, db: Session = Depends(database.get_db)):
+async def read_category_entry(entry_id: int, response: Response, db: Session = Depends(database.get_db)):
     db_entry = archive.get_entry(db=db, entry_id=entry_id)
     if db_entry is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Entry not found")
@@ -46,12 +46,12 @@ def read_category_entry(entry_id: int, response: Response, db: Session = Depends
 
 
 @router.get("/entries", status_code=200, response_model=list[schemas.Entry])
-def read_entries(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
+async def read_entries(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
     db_entries = archive.get_entries(db=db, skip=skip, limit=limit)
     return db_entries
 
 
 # will read post documents for entries from NoSQL, to be done later
 @router.get("/categories/{category_id}/entries/{entry_id}/post", status_code=200)
-def read_category_entry_post(category_id: int, entry_id: int, response: Response):
+async def read_category_entry_post(category_id: int, entry_id: int, response: Response):
     return {}
